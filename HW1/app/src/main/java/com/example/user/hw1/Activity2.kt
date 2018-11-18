dd6619e514e131301a7bb612a6286cbd9b705a23
vanes96 <vanesdacha@mail.ru>
2018-11-18 19:44:17 +0300

    HW1

--- HW1/app/src/main/java/com/example/user/hw1/Activity2.kt
+++ HW1/app/src/main/java/com/example/user/hw1/Activity2.kt
@@ -9,17 +9,13 @@ import java.util.*
 
 class Activity2 : AppCompatActivity()
 {
-    private val secondsValueKey: String = "secondsValueKey"
-    private val isTimerStartedKey: String = "isTimerStartedKey"
-    private val timeToStart = 1000
-    private val shortTimeToStart = 1000
-    private val ten: Int = 10
-    private val hundred: Int = 100
+    private val currentTimeKey: String = "currentTimeKey"
+    private val totalTime = 1000
+    private val startTime = 975
     private val second: Int = 1000
-    private var secondsValue: Int = 0
-    private var secondsText: TextView? = null
-    private var button: Button? = null
-    private var isTimerStarted: Boolean = false
+    private val hundred: Int = 100
+    private val ten: Int = 10
+    private var currentTime: Int = startTime
     private var numbers: HashMap<Int, String> = hashMapOf(
             0 to "ноль", 1 to "один", 2 to "два", 3 to "три", 4 to "четыре", 5 to "пять",
             6 to "шесть", 7 to "семь", 8 to "восемь", 9 to "девять", 10 to "десять",
@@ -29,96 +25,104 @@ class Activity2 : AppCompatActivity()
             80 to "восемьдесят", 90 to "девяносто", 100 to "сто", 200 to "двести",  300 to "триста",
             400 to "четыреста",  500 to "пятьсот",  600 to "шестьсот",  700 to "семьсот", 800 to "восемьсот",
             900 to "девятьсот",  1000 to "тысяча")
-
+    private var currentTimeText: TextView? = null
+    private var button: Button? = null
     var timer: CountDownTimer? = null
 
+    // ====================== Overrides ======================
     override fun onCreate(savedInstanceState: Bundle?)
     {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity2)
-        secondsText = findViewById(R.id.textView)
+        currentTimeText = findViewById(R.id.textView)
         button = findViewById(R.id.button)
         button?.setOnClickListener {
-            if (isTimerStarted)
+            if (isTimerStarted())
                 stopTimer()
             else
-                startTimer(shortTimeToStart)
+                startTimer(startTime)
         }
 
         if (savedInstanceState != null)
         {
-            isTimerStarted = savedInstanceState.getBoolean(isTimerStartedKey)
-            secondsValue = savedInstanceState.getInt(secondsValueKey)
-            secondsText?.text = getNumberName(secondsValue)
+            currentTime = savedInstanceState.getInt(currentTimeKey)
+            updateTimerText()
 
-            if (secondsValue == timeToStart)
-                isTimerStarted = false
-
-            if(isTimerStarted)
-                startTimer(timeToStart - secondsValue)
+            if(isTimerStarted())
+                startTimer(currentTime)
+            else
+                updateTimerText()
         }
         else
         {
-            timer = createTimer(shortTimeToStart)
+            timer = createTimer(totalTime - startTime)
         }
     }
 
     override fun onSaveInstanceState(outState: Bundle)
     {
-        outState.putBoolean(isTimerStartedKey, isTimerStarted)
-        outState.putInt(secondsValueKey, secondsValue)
+        outState.putInt(currentTimeKey, currentTime)
         super.onSaveInstanceState(outState)
     }
 
-    private fun createTimer(time: Int): CountDownTimer
-    {
-        var timer = object : CountDownTimer(time * second.toLong(), second.toLong())
-        {
-            override fun onTick(ticks: Long)
-            {
-                secondsValue = timeToStart - ticks.toInt() / second  // timeToStart - секунд всего
-                secondsText?.text = getNumberName(secondsValue)
-            }
-
-            override fun onFinish() {
-                isTimerStarted = false
-                button?.text = "Start"
-                timer?.cancel()
-            }
-        }
-        return timer
-    }
-
-    private fun startTimer(time: Int)
-    {
-        timer = createTimer(time)
-        timer?.start()
-        isTimerStarted = true
-        button?.text = "Stop"
-    }
-
-    private fun stopTimer()
-    {
-        isTimerStarted = false
-        button?.text = "Start"
-        timer?.cancel()
-    }
-
     override fun onStop() {
-        var lastState = isTimerStarted
+        var lastState = currentTime
         stopTimer()
-        isTimerStarted = lastState
-        if (isTimerStarted)
-            button?.text = "Stop"
+        currentTime = lastState
+        updateTimerText()
+        if (isTimerStarted())
+            setButtonText("Stop")
         super.onStop()
     }
 
     override fun onStart() {
         super.onStart()
-        if (isTimerStarted) {
+        if (isTimerStarted()) {
             timer?.cancel()
-            startTimer(timeToStart - secondsValue)
+            startTimer(currentTime)
         }
+    }
+    // =======================================================
+
+    private fun isTimerStarted(): Boolean = currentTime in (startTime + 1)..(totalTime - 1)
+
+    private fun createTimer(time: Int): CountDownTimer = object : CountDownTimer(time * second.toLong(), second.toLong())
+    {
+        override fun onTick(ticks: Long)
+        {
+            currentTime = totalTime - ticks.toInt() / second  // totalTime - секунд всего
+            updateTimerText()
+        }
+
+        override fun onFinish() {
+            setButtonText("Start")
+            timer?.cancel()
+        }
+    }
+
+    private fun startTimer(time: Int)
+    {
+        timer = createTimer(totalTime - time)
+        timer?.start()
+        setButtonText("Stop")
+    }
+
+    private fun stopTimer()
+    {
+        currentTime = startTime
+        setButtonText("Start")
+        updateTimerText()
+        timer?.cancel()
+    }
+
+    private fun updateTimerText()
+    {
+        currentTimeText?.text = getNumberName(currentTime)
+    }
+
+    private fun setButtonText(text: String)
+    {
+        button?.text = text
     }
 
     fun getNumberName(time: Int): String
@@ -148,6 +152,3 @@ class Activity2 : AppCompatActivity()
         return numberName
     }
 }
-
-
-
